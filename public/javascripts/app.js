6363ab40a11d0f7bef464bc514a7ab178f2b9ded
Kenji <kenjim83@gmail.com>
2019-08-16 23:26:04 -0700

    got websockets working woohoo! can update code and output in real time across all clients

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,11 +4,17 @@ const RUN_BUTTON = "#run-button";
 const OUTPUT_TEXTAREA = "#code-output";
 const PYTHON_EXAMPLE_CODE = "def sayHello():\n  print(\"hello world!\")\n\nsayHello()";
 const CODE_MIRROR_ENTRY_ID = "code-mirror-entry";
+const DEV_WS_HOST = "ws://localhost:8080";
+const CHANGE_FROM_SET_VALUE = "setValue";
+
+const EXEC_CODE = "XC: "; // Execute code - Run code then update on all clients
+const UPDATE_CODE = "UC: "; // Update code - Update code on all clients
+const UPDATE_OUTPUT = "UO: "; // Update output - Update executed code output on all clients
 
 const app = {
   init() {
     $(RUN_BUTTON)
-      .click(this.onRunButtonClick.bind(this));
+      .click(this.onExecuteCode.bind(this));
     $(document)
       .keydown(this.onKeyPress.bind(this))
       .keypress(this.onKeyPress.bind(this));
@@ -16,32 +22,45 @@ const app = {
     this.codeMirror = CodeMirror(document.getElementById(CODE_MIRROR_ENTRY_ID), {
       value: PYTHON_EXAMPLE_CODE,
       lineNumbers: true,
-      mode: "text/x-python",
+      mode: "python",
     });
+
+    this.$outputTextarea = $(OUTPUT_TEXTAREA);
+
+    this.wsClient = new WebSocket(DEV_WS_HOST);
+
+    this.codeMirror.on('change', (instance, { origin }) => {
+      if (origin !== CHANGE_FROM_SET_VALUE) {
+        const rawCode = instance.getValue();
+        this.wsClient.send(UPDATE_CODE + rawCode);
+      }
+    });
+
+    this.wsClient.onmessage = (messageObj) => {
+      const eventCode = messageObj.data.slice(0,4);
+      const data = messageObj.data.slice(4);
+
+      if (eventCode === UPDATE_CODE) {
+        this.codeMirror.setValue(data);
+      }
+
+      if (eventCode === UPDATE_OUTPUT) {
+        this.$outputTextarea.text(data);
+      }
+    }
   },
 
-  onRunButtonClick() {
+  onExecuteCode() {
     const rawCode = this.codeMirror.getValue();
-
-    this.sendCodeToServer({ rawCode })
-      .then(resp => {
-        const output = Array.isArray(resp.output)
-          ? resp.output.join("\n")
-          : resp.output;
-        $(OUTPUT_TEXTAREA).text(output);
-      })
-      .fail(resp => {
-        $(OUTPUT_TEXTAREA).text(resp.responseText);
-      })
+    this.wsClient.send(EXEC_CODE + rawCode);
   },
 
   onKeyPress(e) {
     if (e.keyCode == 13 && e.shiftKey) {
-      this.onRunButtonClick();
+      this.onExecuteCode();
       e.preventDefault();
     }
   },
-
 
   sendCodeToServer({ rawCode }){
     return $.ajax({
@@ -51,8 +70,6 @@ const app = {
       dataType: 'json',
     });
   },
-
-
 };
 
 
---
+++ server.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const WebSocket = require('ws');
+const bodyParser = require('body-parser');
+const exphbs  = require('express-handlebars');
+const request = require('request');
+const { PythonShell } = require('python-shell');
+const uuid = require('uuid/v1');
+
+const HTTP_PORT = 5000;
+const WS_PORT = 8000;
+
+
+/* WebSocket message protocol
+ * "<2 char event code>: <data>"
+ *
+ * Example:
+ * "XC: x = 1n\.."
+ * "UC: x = 1n\.."
+ */
+const EXEC_CODE = "XC: "; // Execute code - Run code then update on all clients
+const UPDATE_CODE = "UC: "; // Update code - Update code on all clients
+const UPDATE_OUTPUT = "UO: "; // Update output - Update executed code output on all clients
+const OPEN_READY_STATE = 1; // 'readyState' when socket connection is open
+const CLOSED_READY_STATE = 3; // 'readyState' when socket connection is closed
+
+const app = express();
+app.set('port', (process.env.PORT || HTTP_PORT));
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+const wsClients = {};
+
+function removeClosedConnections(wsClients) {
+  Object.keys(wsClients).forEach(key => {
+    const client = wsClients[key];
+    if (client.readyState !== OPEN_READY_STATE) {
+      // close connection
+      client.close();
+      delete wsClients[key];
+    }
+  })
+}
+
+wss.on('connection', function connection(ws) {
+  // save new ws connect in cache with random uuid as key
+  const userId = uuid();
+  wsClients[userId] = ws;
+  // remove any disconnected client connections. every page refresh closes previous connection.
+  removeClosedConnections(wsClients)
+  ;
+  console.log({clients: Object.keys(wsClients)});
+
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+
+    const eventCode = message.slice(0,4);
+    const data = message.slice(4);
+
+    if (eventCode === EXEC_CODE) {
+      PythonShell.runString(data, null, function (err, output) {
+        console.log({output})
+        let outputToClient = "";
+        if (err) {
+          outputToClient = err.message;
+        } else if (output) {
+          outputToClient = Array.isArray(output) ? output.join('\n') : output;
+        }
+        Object.keys(wsClients).forEach(currUserId => {
+          const wsClient = wsClients[currUserId];
+          wsClient.send(UPDATE_OUTPUT + outputToClient);
+        });
+      });
+    }
+
+    if (eventCode === UPDATE_CODE) {
+      Object.keys(wsClients).forEach(currUserId => {
+        // only update code for *other* clients
+        if (currUserId !== userId) {
+          const wsClient = wsClients[currUserId];
+          wsClient.send(UPDATE_CODE + data);
+        }
+      });
+    }
+
+  });
+});
+
+wss.on('close', function close(ws) {
+  console.log('close!')
+});
+
+app.use(express.static(__dirname + '/public'));
+// for parsing application/json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// use Handlebars as templating engine instead of Express default one
+app.engine('handlebars', exphbs({defaultLayout: 'base'}));
+app.set('view engine', 'handlebars');
+
+// define the app routes
+app.get('/', function(req, res) {
+  res.render('pages/home');
+});
+
+// make a 404 error page
+app.use(function (req, res) {
+	res.status(404);
+	res.render('pages/404');
+});
+
+// handle other errors
+app.use(function (err, req, res) {
+  res.status(500);
+  res.render('pages/error', { error: err });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
